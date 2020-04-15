28b63211b9dcb4824d9bcfe0fa48f000a8529c39
DThaiPome <r.gehrlein154@gmail.com>
2020-04-15 13:53:56 -0400

    Menu Control:
    MenuControl script should be attached to any Canvas or other Menu object.
    Attach ClickForMenu script to object with a Collider, and refer it to the MenuControl script of a menu. When the object is clicked, the menu will do whatever is specified in the "Menu Action" field.

---
+++ Games&Society/Assets/Scripts/Menus/ClickForMenu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickForMenu : MonoBehaviour
+{
+    [SerializeField]
+    private MenuController mc;
+
+    [Tooltip("For menu action: Use \"open\", \"close\", or \"switch\"")]
+    [SerializeField]
+    private string menuAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.instance.onClickEvent += this.onClick;
+    }
+
+    private void onClick(Transform t)
+    {
+        if (t.Equals(this.transform))
+        {
+            switch(this.menuAction)
+            {
+                case "open":
+                    this.openMenu();
+                    break;
+                case "close":
+                    this.closeMenu();
+                    break;
+                case "switch":
+                    this.switchMenu();
+                    break;
+            }
+        }
+    }
+
+    private void switchMenu()
+    {
+        this.mc.switchMenu();
+    }
+
+    private void openMenu()
+    {
+        this.mc.open();
+    }
+
+    private void closeMenu()
+    {
+        this.mc.close();
+    }
+}
