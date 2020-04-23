cc767b0fa284b13f540a7a6ea33a9369edcbd444
DThaiPome <r.gehrlein154@gmail.com>
2020-04-23 17:42:35 -0400

    Displays days completed

---
+++ Games&Society/Assets/Scripts/GameOverDayCount.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverDayCount : MonoBehaviour
+{
+    [SerializeField]
+    Text text;
+
+    void Start()
+    {
+        EventManager.instance.onNextDayEvent += this.updateDay;
+        this.updateDay(0);
+    }
+
+    private void updateDay(int day)
+    {
+        this.text.text = "Days Studied: " + day;
+    }
+}
